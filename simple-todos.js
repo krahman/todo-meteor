6c48a539ffafc009c8c36d1eae186817a942237a
Khalilur Rahman <khal.rahman@gmail.com>
2015-05-21 23:25:03 +0800

    Added basic user authentication

--- simple-todos.js
+++ simple-todos.js
@@ -23,7 +23,9 @@ if (Meteor.isClient) {
 
       Tasks.insert({
         text: text,
-        createdAt: new Date()
+        createdAt: new Date(),
+        owner: Meteor.userId(),
+        username: Meteor.user().username
       });
 
       event.target.text.value = "";
@@ -43,4 +45,9 @@ if (Meteor.isClient) {
       Tasks.remove(this._id);
     }
   });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 }
+
