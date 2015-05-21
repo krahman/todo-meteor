8d193f66de3d40b54b882d9fd4effde08dff0863
Khalilur Rahman <khal.rahman@gmail.com>
2015-05-21 22:44:47 +0800

    Added temporary UI state

--- simple-todos.js
+++ simple-todos.js
@@ -3,7 +3,17 @@ Tasks = new Mongo.Collection('tasks');
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function() {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    "hideCompleted": function() {
+      return Session.get("hideCompleted");
+    },
+    "incompleteCount": function() {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -19,6 +29,9 @@ if (Meteor.isClient) {
       event.target.text.value = "";
 
       return false;
+    },
+    "change .hide-completed input": function(event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
