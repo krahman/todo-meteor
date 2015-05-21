aa6485bbb383f568f44a51fe21269484b256a182
Khalilur Rahman <khal.rahman@gmail.com>
2015-05-21 21:43:13 +0800

    Added form and input event

--- simple-todos.js
+++ simple-todos.js
@@ -3,7 +3,22 @@ Tasks = new Mongo.Collection('tasks');
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function() {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function(event) {
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      event.target.text.value = "";
+
+      return false;
     }
   });
 }
